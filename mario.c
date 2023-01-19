898e49192d425efea323dd6b270dde387729b3e3
Mrt009 <matrikaregmi09@gmail.com>
2023-01-19 21:21:57 +0545

    inital commit

---
+++ mario.c
@@ -0,0 +1,36 @@
+
+#include <cs50.h>
+#include <stdio.h>
+
+// function for generating bricks
+void generateBricks(int height)
+{
+    for (int y = 0; y < height; y++)
+    {
+        //spacer
+        for (int x = (height - 1); x > y; x--)
+        {
+            printf(" ");
+        }
+
+        // brick
+        for (int x = 0; x <= y; x++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+
+/*  M A I N  */
+int main(void)
+{
+    int usrString;
+    do
+    {
+        usrString = get_int("Height: ");
+    }
+    while (usrString > 8 || usrString <= 0);
+    generateBricks(usrString);
+}
